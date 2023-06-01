d952dc7107276d5070555c7c396be1f82a6a35e7
summer-o3o <ho_ho95@naver.com>
2023-06-01 20:47:52 +0900

    add : 초기셋팅

---
+++ src/Router.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignIn from './pages/SignIn/SignIn';
+import SignUp from './pages/SignUp/SignUp';
+import Todo from './pages/Todo/Todo';
+import NotFound from './pages/NotFound/NotFound';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/todo" element={<Todo />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
---
+++ src/pages/SignIn/SignIn.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './SignIn.scss';
+
+const SignIn = () => {
+  return <div>signin</div>;
+};
+
+export default SignIn;
