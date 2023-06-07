0eca3a21c3b3f6387a29e8b4cf212c9b8a054a86
summer-o3o <ho_ho95@naver.com>
2023-06-07 15:25:25 +0900

    update : 수정취소 오류 수정

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -3,12 +3,13 @@ import React, { useState, useEffect } from 'react';
 const TodoList = ({ todoList, token, setTodoList, id, todo, isCompleted }) => {
   const [modify, setModify] = useState(false);
   const [isChecked, setIsChecked] = useState(false);
+  const [InputValue, setInputValue] = useState(todo);
 
   useEffect(() => {
     setIsChecked(isCompleted);
   }, [isCompleted]);
 
-  const handleUpdate = (id, updatedTodo, updatedIsCompleted) => {
+  const handleUpdate = (id, updatedIsCompleted) => {
     fetch(`https://www.pre-onboarding-selection-task.shop/todos/${id}`, {
       method: 'PUT',
       headers: {
@@ -16,19 +17,22 @@ const TodoList = ({ todoList, token, setTodoList, id, todo, isCompleted }) => {
         Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify({
-        todo: updatedTodo,
+        todo: InputValue,
         isCompleted: updatedIsCompleted, // 체크박스 상태 서버에 보내기
       }),
     })
       .then(response => response.json())
-      .then(data => {
-        console.log(data);
+      .then(() => {
+        setModify(false);
+        setTodoList(prevList =>
+          prevList.map(item =>
+            item.id === id ? { ...item, todo: InputValue } : item
+          )
+        );
       })
       .catch(error => {
         console.error(error);
       });
-
-    setModify(false);
   };
 
   const handleDelete = id => {
@@ -59,12 +63,16 @@ const TodoList = ({ todoList, token, setTodoList, id, todo, isCompleted }) => {
     if (modify) {
       return;
     } else {
-      handleUpdate(id, todo, updatedIsCompleted);
+      handleUpdate(id, updatedIsCompleted);
     }
   };
 
   const handleModify = () => {
-    setModify(true);
+    setModify(!modify);
+  };
+
+  const handleCancel = () => {
+    setModify(false);
   };
 
   return (
@@ -81,15 +89,9 @@ const TodoList = ({ todoList, token, setTodoList, id, todo, isCompleted }) => {
             <input
               className="inp_todolist"
               type="text"
-              value={todo}
+              value={InputValue}
               data-testid="modify-input"
-              onChange={e =>
-                setTodoList(prevList =>
-                  prevList.map(item =>
-                    item.id === id ? { ...item, todo: e.target.value } : item
-                  )
-                )
-              }
+              onChange={e => setInputValue(e.target.value)}
             />
           </span>
         ) : (
@@ -102,9 +104,7 @@ const TodoList = ({ todoList, token, setTodoList, id, todo, isCompleted }) => {
           type="button"
           data-testid={modify ? 'submit-button' : 'modify-button'}
           onClick={
-            modify
-              ? () => handleUpdate(id, todo, isChecked)
-              : () => handleModify(id)
+            modify ? () => handleUpdate(id, isChecked) : () => handleModify(id)
           }
         >
           {modify ? '제출' : '수정'}
@@ -113,7 +113,7 @@ const TodoList = ({ todoList, token, setTodoList, id, todo, isCompleted }) => {
           className="btn_delete"
           type="button"
           data-testid={modify ? 'cancel-button' : 'delete-button'}
-          onClick={modify ? () => setModify(false) : () => handleDelete(id)}
+          onClick={modify ? () => handleCancel() : () => handleDelete(id)}
         >
           {modify ? '취소' : '삭제'}
         </button>
