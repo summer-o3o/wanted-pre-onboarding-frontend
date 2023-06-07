2d53b70727c8eda853d1828bf7a1d41713862e67
summer-o3o <ho_ho95@naver.com>
2023-06-07 13:17:50 +0900

    add : todo 리스트 내용 입력 form 기능 추가 add : todo 리스트 체크박스 체크여부 기능 추가 add : todo 리스트 수정, 삭제, 수정 취소 기능 추가 update : todo 리스트 & common 스타일 수정

---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,121 @@
+import React, { useState, useEffect } from 'react';
+
+const TodoList = ({ todoList, token, setTodoList, id, todo, isCompleted }) => {
+  const [modify, setModify] = useState(false);
+  const [isChecked, setIsChecked] = useState(false);
+
+  useEffect(() => {
+    setIsChecked(isCompleted);
+  }, [isCompleted]);
+
+  const handleUpdate = (id, updatedTodo, updatedIsCompleted) => {
+    fetch(`https://www.pre-onboarding-selection-task.shop/todos/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        todo: updatedTodo,
+        isCompleted: updatedIsCompleted, // 체크박스 상태 서버에 보내기
+      }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+
+    setModify(false);
+  };
+
+  const handleDelete = id => {
+    fetch(`https://www.pre-onboarding-selection-task.shop/todos/${id}`, {
+      method: 'DELETE',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then(response => {
+        if (response.ok) {
+          const updatedTodoList = todoList.filter(
+            todoItem => todoItem.id !== id
+          );
+          setTodoList(updatedTodoList);
+        } else {
+          throw new Error('Todo 삭제에 실패했습니다.');
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  const handleChecked = () => {
+    const updatedIsCompleted = !isChecked;
+    setIsChecked(updatedIsCompleted);
+
+    // 체크박스 상태와 함께 업데이트 요청을 서버에 전송
+    handleUpdate(id, todo, updatedIsCompleted);
+  };
+
+  const handleModify = () => {
+    setModify(true);
+  };
+
+  return (
+    <li key={id}>
+      <label className="lnp_todo">
+        <input
+          className="check_todo"
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleChecked}
+        />
+        {modify ? (
+          <span className="wrap_todolist">
+            <input
+              className="inp_todolist"
+              type="text"
+              value={todo}
+              data-testid="modify-input"
+              onChange={e =>
+                setTodoList(prevList =>
+                  prevList.map(item =>
+                    item.id === id ? { ...item, todo: e.target.value } : item
+                  )
+                )
+              }
+            />
+          </span>
+        ) : (
+          <span className="txt_todo">{todo}</span>
+        )}
+      </label>
+      <div className="wrap_btn">
+        <button
+          className="btn_modify"
+          type="button"
+          data-testid={modify ? 'submit-button' : 'modify-button'}
+          onClick={
+            modify ? () => handleUpdate(id, todo) : () => handleModify(id)
+          }
+        >
+          {modify ? '제출' : '수정'}
+        </button>
+        <button
+          className="btn_delete"
+          type="button"
+          data-testid={modify ? 'cancel-button' : 'delete-button'}
+          onClick={modify ? () => setModify(false) : () => handleDelete(id)}
+        >
+          {modify ? '취소' : '삭제'}
+        </button>
+      </div>
+    </li>
+  );
+};
+
+export default TodoList;
--- src/pages/Todo/Todo.js
+++ src/pages/Todo/Todo.js
@@ -1,18 +1,58 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Title from '../../components/Title/Title';
+import TodoList from '../../components/TodoList/TodoList';
 import './Todo.scss';
 
 const Todo = () => {
+  const [todoList, setTodoList] = useState([]);
+  const [todo, setTodo] = useState('');
+
+  const token = localStorage.getItem('token');
   const navigate = useNavigate();
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
-
     if (!token) {
       alert('로그인시 이용 가능합니다. 로그인 해주세요.');
       navigate('/signin');
     }
+  }, []);
+
+  const handleTodoChange = e => {
+    setTodo(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    fetch('https://www.pre-onboarding-selection-task.shop/todos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json', //필수로 넣어야함
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        todo: todo,
+      }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        setTodoList([...todoList, data]); // 새로운 투두 아이템을 기존 투두 리스트에 추가
+        setTodo('');
+      });
+  };
+
+  useEffect(() => {
+    fetch('https://www.pre-onboarding-selection-task.shop/todos', {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then(response => response.json())
+      .then(data => {
+        setTodoList(data); // 데이터를 그대로 설정
+      });
   }, []);
 
   return (
@@ -21,12 +61,19 @@ const Todo = () => {
         title="해야할일을 적어주세요!"
         desc="해야할일을 하나씩 적으면서 기록해보세요."
       />
-      <form method="post" action="#none" className="form_todo">
+      <form
+        method="post"
+        action="#none"
+        className="form_todo"
+        onSubmit={handleSubmit}
+      >
         <legend className="screen_out">해야 할 일 입력</legend>
         <input
           className="inp_todo"
           data-testid="new-todo-input"
           placeholder="해야 할 일을 입력해주세요"
+          onChange={handleTodoChange}
+          value={todo}
         />
         <button
           className="btn_add"
@@ -36,33 +83,24 @@ const Todo = () => {
           추가
         </button>
       </form>
-      <strong className="screen_out">해야 할 일 목록</strong>
-      <ul className="list_todo">
-        <li>
-          <label className="lnp_todo">
-            <input className="check_todo" type="checkbox" />
-            <span className="txt_todo">
-              일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십
-            </span>
-          </label>
-          <div className="wrap_btn">
-            <button
-              className="btn_modify"
-              type="button"
-              data-testid="modify-button"
-            >
-              수정
-            </button>
-            <button
-              className="btn_delete"
-              type="button"
-              data-testid="delete-button"
-            >
-              삭제
-            </button>
-          </div>
-        </li>
-      </ul>
+      {todoList.length >= 1 && (
+        <>
+          <strong className="screen_out">해야 할 일 목록</strong>
+          <ul className="list_todo">
+            {todoList.map(({ todo, id, isCompleted }) => (
+              <TodoList
+                todoList={todoList}
+                token={token}
+                key={id}
+                id={id}
+                todo={todo}
+                isCompleted={isCompleted}
+                setTodoList={setTodoList}
+              />
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 };
